0a62dc94386166519420cedd5f85fdcb51681278
Blackprincex <appiahboaduprince@gmail.com>
2022-12-12 09:39:43 +0000

    add matrix custom typedefinitions for operations and testing

--- src/data-structures/Arrays/mul-dim-arrays.c
+++ src/data-structures/Arrays/mul-dim-arrays.c
@@ -16,3 +16,27 @@ void printMatrix(const ROW_t *pMat, int nRows) {
       printf("%6.2f", pMat[r][cl]);
   }
 }
+
+typedef int Matrix[16][16];
+
+void p_matrix_memory_data(Matrix *pMat, int mROWS, int mCOLS) {
+  for (int row=0; row < mROWS; ++row) {
+    for (int col=0; col < mCOLS; ++col) {
+      // shift the null values in the intialized mul-dim array
+      // by 0x10 bits so we can see the memory row values clearly
+      // when logged on the console.
+      *pMat[row][col] = (row << 16) | col;
+    }
+  }
+
+  printf("Sizeof(pMat) = %lu\n", sizeof(pMat));
+}
+
+void example() {
+  const int mROWs = 3;
+  const int mCOLS = 4;
+  Matrix example[mROWs][mCOLS];
+
+  p_matrix_memory_data(*example, mROWs, mCOLS);
+
+}
