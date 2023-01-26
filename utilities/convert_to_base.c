501847e2b44671f1b57946ab9648a4056836bf02
Appiah Boadu Prince Kwabena <appiahboaduprince@gmail.com>
2023-01-26 22:00:02 +0000

    Add Base Converter program (GCD approach) to repo

---
+++ utilities/convert_to_base.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+  int num = 101;
+  int base = 2;
+
+  while (true) {
+    int quo = num / base;
+    int r = num % base;
+
+    printf("quotient=%i, remainder=%i\n", quo, r);
+    num = quo;
+
+    if (num == 0) break;
+  }
+  return 0;
+}
