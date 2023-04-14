1a1faecea5a6f93e9b3026be4b1299b508db3dfe
blackprince001 <appiahboaduprince@gmail.com>
2023-04-14 08:03:14 +0000

    Add named function parameters trick to Concepts

---
+++ c-concepts/named_function_parameters.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+struct _foo_args {
+    int num;
+    const char* text;
+};
+
+#define foo(...) _foo((struct _foo_args) { __VA_ARGS__ })
+int _foo(struct _foo_args args)
+{
+    puts(args.text);
+    return args.num * 2;
+}
+
+int main(void)
+{
+    int result = foo(.text = "Hello!", .num = 8);
+    return 0;
+}
